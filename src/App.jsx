33561439ebde5f5cc968b327d9929dad8f15573c
ikhwannaufal <ikhwannaufal@gmail.com>
2024-01-03 22:26:14 +0800

    add experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import "./App.css";
 
+function Experience() {
+  return (
+    <>
+      <h3>Software Developer</h3>
+      <h4>Sigma Lab Sdn Bhd</h4>
+    </>
+  );
+}
+
 function App() {
   return (
     <>
       <h1>Michael Scott</h1>
       <h2>Software Developer</h2>
+      <Experience />
     </>
   );
 }
